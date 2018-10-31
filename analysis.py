718f60bb6dcf86896d3eb18df4075691019d0a67
Michael <michaeljomori@gmail.com>
2018-10-30 18:18:45 -1000

    First Commit

---
+++ analysis.py
@@ -0,0 +1,3 @@
+import json
+with open('yelp_academic_dataset_review.json') as f:
+    data = json.load(f)
---
+++ conversion.py
@@ -0,0 +1,5 @@
+import json
+
+with open("yelp_academic_dataset_business.json", encoding="utf8") as f:
+    data = json.load(f)
+
