f88037ce3b1441a4118c8a8b196b3ddc903eba89
astroboy1 <omorim@hawaii.edu>
2018-11-10 14:48:21 -1000

    Analyze JSON

--- analysis.py
+++ analysis.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# https://seaborn.pydata.org/tutorial.html
+# Folium for map visualization
+
 # https://stackoverflow.com/questions/30088006/loading-a-file-with-more-than-one-line-of-json-into-pythons-pandas
+# https://www.dataquest.io/blog/python-json-tutorial/
+# Dataframe subsetting: https://medium.com/dunder-data/selecting-subsets-of-data-in-pandas-6fcd0170be9c
+# Dataframe string: https://stackoverflow.com/questions/11350770/pandas-dataframe-select-by-partial-string
 
 
 def main():
     df = pd.read_json("yelp_dataset/yelp_academic_dataset_business.json", lines=True)
     print(df.head)
+    food_columns = df[df['categories'].str.contains("Food")==True]
+    print("Number of restaurants", len(food_columns))
+    df['city'].value_counts()
 
 
 if __name__ == "__main__":
